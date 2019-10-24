faab6c7f189daff33560c0c0aa7b6441bed6c423
Daniel Le'Sage <DLesage25@users.noreply.github.com>
2019-10-23 22:52:54 -0600

    formatting files with prettier

--- engines/AudioDAO.js
+++ engines/AudioDAO.js
@@ -2,23 +2,6 @@ import { Audio } from 'expo-av';
 import MetronomeOnNote from '../assets/audio/metronome_on_note.wav';
 import MetronomeOffNote from '../assets/audio/metronome_off_note.wav';
 
-//deprecated, unused TODO - remove this
-export const PlayAudio = async soundURI => {
-    try {
-        //TODO - change this reference, assuming I may need formats other
-        // than mp3 in the future
-        let { isLoaded } = await sound_metronomeActive.getStatusAsync();
-        if (!isLoaded) await sound_metronomeActive.loadAsync(MetronomeOnNote);
-
-        await sound_metronomeActive.replayAsync(); //TODO when to use playAsync?
-
-        return true;
-    } catch (error) {
-        console.log(`Error when playing audio ${error}`);
-        return false;
-    }
-};
-
 //unused - should I load audio files before playing?
 const LoadAudioFiles = async soundFiles => {
     try {
