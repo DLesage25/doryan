89fa28817575641cbb7eed47c7f21dae7c6a29a0
Daniel Le'Sage <DLesage25@users.noreply.github.com>
2019-10-26 15:50:40 -0600

    adding first components to looper screen

--- screens/LooperScreen.js
+++ screens/LooperScreen.js
@@ -8,8 +8,99 @@ import {
     Slider,
 } from 'react-native';
 import CustomButton from '../components/CustomButton';
+import CustomButtonGroup from '../components/CustomButtonGroup';
 
 import LooperStyles from '../styles/LooperStyles';
+
+const LoopLengthControl = () => {
+    const options = ['5s', '10s', '15s'];
+
+    return (
+        <View style={styles.controlContainer}>
+            <Text style={styles.subTitle}> Loop length </Text>
+            <CustomButtonGroup
+                options={options}
+                onPress={selected => {
+                    dispatch({
+                        type: 'CHANGE_ACCENT',
+                        payload: {
+                            index: selected,
+                            value: options[selected],
+                        },
+                    });
+                }}
+                selectedIndex={0}
+            />
+        </View>
+    );
+};
+
+const LooperButtons = () => {
+    return (
+        <View style={styles.centeredFlex}>
+            <CustomButton
+                text={'Play All'}
+                colorSet="primary"
+                onPress={() => {
+                    console.log('loop');
+                }}
+            />
+            <CustomButton
+                text={'Rec/Dub'}
+                colorSet="primary"
+                onPress={() => {
+                    console.log('loop');
+                }}
+            />
+        </View>
+    );
+};
+
+const IndividualLoopControl = () => {
+    return (
+        <View style={styles.controlContainer}>
+            <Text style={styles.subTitle}> Loop 1 </Text>
+            <View style={styles.individualLoopControls}>
+                <View>
+                    {/* <Text style={styles.subTitle}> Volume </Text> */}
+                    <View style={styles.sliderContainer}>
+                        <Slider
+                            maximumValue="10"
+                            minimumValue="1"
+                            step={1}
+                            value={5}
+                            minimumTrackTintColor="#f1c40f"
+                            maximumTrackTintColor="#2c3e50"
+                            onSlidingComplete={newTempo => {
+                                dispatch({
+                                    type: 'CHANGE_TEMPO',
+                                    payload: newTempo,
+                                });
+                            }}
+                        />
+                    </View>
+                </View>
+                <View style={styles.centeredRow}>
+                    {/* <Text style={styles.subTitle}> Audio </Text> */}
+                    <CustomButton
+                        text={'Play'}
+                        colorSet="secondary"
+                        onPress={() => {
+                            console.log('loop');
+                        }}
+                    />
+                    <CustomButton
+                        text={'Delete'}
+                        colorSet="secondary"
+                        onPress={() => {
+                            console.log('loop');
+                        }}
+                    />
+                </View>
+            </View>
+        </View>
+    );
+};
 
 const LooperScreen = () => {
     return (
@@ -22,21 +113,9 @@ const LooperScreen = () => {
                 <Text style={styles.title}> Looper </Text>
             </View>
             <View style={styles.toolContainer}>
-                <CustomButton
-                    text="Tap Tempo"
-                    colorSet="secondary"
-                    onPress={() => {
-                        console.log('tap tempo');
-                    }}
-                />
-
-                <CustomButton
-                    text={'Loop'}
-                    colorSet="primary"
-                    onPress={() => {
-                        console.log('loop');
-                    }}
-                />
+                <LoopLengthControl />
+                <IndividualLoopControl />
+                <LooperButtons />
             </View>
         </ScrollView>
     );
--- styles/LooperStyles.js
+++ styles/LooperStyles.js
@@ -8,13 +8,16 @@ export default {
         marginBottom: 10,
     },
     toolContainer: {
-        // flex: 1,
         backgroundColor: '#34495e',
         marginTop: 0,
         height: 1000,
-        // padding: 10,
         alignItems: 'center',
         marginBottom: 0,
+    },
+    controlContainer: {
+        alignItems: 'center',
+        paddingTop: 10,
+        marginHorizontal: 10,
     },
     welcomeImage: {
         width: 200,
@@ -26,5 +29,30 @@ export default {
         fontSize: 30,
         fontFamily: 'Avenir',
         color: '#34495e',
+    },
+    subTitle: {
+        fontSize: 20,
+        fontFamily: 'Avenir-Medium',
+        color: '#f1c40f',
+    },
+    smallLabel: {
+        fontSize: 16,
+        fontFamily: 'Avenir-Medium',
+        color: '#e67e22',
+        alignSelf: 'center',
+        marginTop: 5,
+    },
+    sliderContainer: {
+        width: 150,
+    },
+    centeredRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+    },
+    individualLoopControls: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
     },
 };
